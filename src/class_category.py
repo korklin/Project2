423dd9bbb4416009bea00965466140c9c91aa922
agaranina <aleksandra.belik74@gmail.com>
2025-07-31 13:13:08 +0700

    переделаны class_made.py class_product.py и class_category.py, test_class_made.py в test_product.py и test_category.py, добавлены дочерние классы Smartphone и LawnGrass

--- src/class_category.py
+++ src/class_category.py
@@ -1,43 +1,5 @@
 
-class Product:
-    name: str
-    description: str
-    __price: float
-    quantity: int
-
-    def __init__(
-        self, name: str, description: str, price: float, quantity: int
-    ) -> None:
-        self.name = name
-        self.description = description
-        self.__price = price
-        self.quantity = quantity
-
-    @property
-    def price(self) -> float:
-        return self.__price
-
-    @price.setter
-    def price(self, value: float) -> None:
-       if value <= 0:
-           print("Цена не должна быть нулевая или отрицательная")
-       else:
-           self.__price = value
-
-    @classmethod
-    def new_product(cls, name: str, description: str, price: float, quantity: int) -> "Product":
-        return cls(name, description, price, quantity)
-
-    def __add__(self, other: 'Product') -> float:
-        if isinstance(other, Product):
-            return (self.price * self.quantity) + (other.price * other.quantity)
-        raise TypeError("Можно складывать только объекты класса Product.")
-
-    def __str__(self) -> str:
-        return f"{self.name} ({self.price} руб., {self.quantity} шт)"
-
-    def __repr__(self) -> str:
-        return self.__str__()
+from src.class_product import Product, product1, product2, product3
 
 
 class Category:
@@ -86,14 +48,6 @@ class Category:
 
 
 if __name__ == "__main__":
-    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
-    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
-
-    print(str(product1))
-    print(str(product2))
-    print(str(product3))
-
     category1 = Category(
         "Смартфоны",
         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
