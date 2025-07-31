5d6f2bcb53998e9d7ba4ee3a1d9075da2ebd4258
agaranina <aleksandra.belik74@gmail.com>
2025-07-31 13:37:15 +0700

    переделан __add__

--- src/class_product.py
+++ src/class_product.py
@@ -1,3 +1,5 @@
+from typing import Self
+
 
 class Product:
     name: str
@@ -28,10 +30,10 @@ class Product:
     def new_product(cls, name: str, description: str, price: float, quantity: int) -> "Product":
         return cls(name, description, price, quantity)
 
-    def __add__(self, other: 'Product') -> float:
-        if isinstance(other, Product):
-            return (self.price * self.quantity) + (other.price * other.quantity)
-        raise TypeError("Можно складывать только объекты класса Product.")
+    def __add__(self, other: Self) -> float:
+        if type(self) is not type(other):
+            raise TypeError(f"Нельзя складывать товары разных типов: {type(self).__name__} и {type(other).__name__}")
+        return (self.price * self.quantity) + (other.__price * other.quantity)
 
     def __str__(self) -> str:
         return f"{self.name} ({self.price} руб., {self.quantity} шт)"
@@ -43,12 +45,18 @@ class Product:
 class Smartphone(Product):
     def __init__(self, name: str, description: str, price: float, quantity: int, efficiency: float, model: str, memory: int, color: str ):
         super().__init__(name, description, price, quantity)
-
+        self.efficiency = efficiency
+        self.model = model
+        self.memory = memory
+        self.color = color
 
 class LawnGrass(Product):
     def __init__(self, name: str, description: str, price: float, quantity: int, country: str, germination_period: str,
                  color: str):
         super().__init__(name, description, price, quantity)
+        self.country = country
+        self.germination_period = germination_period
+        self.color = color
 
 
 
